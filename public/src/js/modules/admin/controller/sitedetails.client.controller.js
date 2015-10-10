19ce9f53764996a2eed042f2a75c56519dfeac08
liliansousa <liliandias1011@gmail.com>
2015-10-10 18:25:34 -0300

    alterações na view portal administrativo - adicionando elementos de navegação na estrutura

---
+++ public/src/js/modules/admin/controller/sitedetails.client.controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('sitedetails').controller('SiteDetailsController', ['$scope', '$http',
+    function($scope, $http) {
+
+	$scope._ = _;
+    $scope.formData = {};
+    $scope.variable = 'Variavél do controller do sitedetails.client.controller.js';
+  }
+]);
