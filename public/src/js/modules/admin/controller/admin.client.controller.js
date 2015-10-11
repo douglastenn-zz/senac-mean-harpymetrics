ce183ea30fb7eae6f0c8149a37545a18374fd873
matheusmf <matheusmf@gmail.com>
2015-10-10 22:08:03 -0300

    alterei a tabela de sites para ser dinâmica.

--- public/src/js/modules/admin/controller/admin.client.controller.js
+++ public/src/js/modules/admin/controller/admin.client.controller.js
@@ -6,5 +6,20 @@ angular.module('admin').controller('AdminController', ['$scope', '$http',
 	$scope._ = _;
     $scope.formData = {};
     $scope.variable = 'Variavél do controller do admin.client.controller.js';
+        
+    function getWebsites() {
+        $http.get('/websites')
+            .success(function(websites) {
+                $scope.websites = websites;
+                $scope.message = {};
+            })
+            .error(function(err) {
+                console.log('Error: ' + err);
+                $scope.message = {
+                   texto: 'Não foi possível obter a lista.'
+                };
+            });
+    }
+    getWebsites();
   }
 ]);
