962a4d062e39cb905b04a15c8886b05a11a33d5e
liliansousa <liliandias1011@gmail.com>
2015-10-12 11:05:56 -0300

    alterando opções de navegação de relatórios - corrigindo menu

--- public/src/js/modules/reports/config/products.client.routes.js
+++ public/src/js/modules/reports/config/products.client.routes.js
@@ -5,7 +5,7 @@ angular.module('products').config(['$stateProvider', '$urlRouterProvider',
 	    $stateProvider
 
         .state('products', {
-            url: '/admin/report/procucts:websiteId',
+            url: '/admin/report/products',
             templateUrl: '/src/js/modules/reports/views/products.client.view.html'
         });
 	}
