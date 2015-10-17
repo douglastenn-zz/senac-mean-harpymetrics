9c03fa96247404a0928ee8c53d122a8b441236e6
matheusmf <matheusmf@gmail.com>
2015-10-17 20:29:56 -0300

    adicionei consulta de produtos mais acessados a view

--- public/src/js/controllers/products.client.controller.js
+++ public/src/js/controllers/products.client.controller.js
@@ -6,6 +6,37 @@ angular.module('products').controller('ProductsController', ['$scope', '$http', 
 	$scope._ = _;
     $scope.formData = {};
     $scope.variable = 'Variavél do controller do products.client.controller.js';
+        
+    function getWebsite() {
+        if($stateParams.websiteId) {
+            $http.get('/websites/' + $stateParams.websiteId)
+            .success(function(website) {
+                $scope.website = website;
+                getMostAcessed(website);
+            })
+            .error(function(err) {
+                console.log('Error: ' + err);
+                $scope.message = {
+                   texto: 'Não foi possível obter a lista.'
+                };
+            });
+        }
+    }
+    getWebsite();
+        
+    function getMostAcessed(website) {
+        console.log('website', website.harpyid);
+        $http.get('/' + website.harpyid + '/product/most-acessed')
+            .success(function(products) {
+                $scope.products = products;
+            })
+            .error(function(err) {
+                console.log('Error: ' + err);
+                $scope.message = {
+                   texto: 'Não foi possível obter a lista.'
+                };
+            });
+    }
 
   }
 ]);
